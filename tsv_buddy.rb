a5fac60cf74427b04020c00ac7fc4e4836a762d4
Daniel Kong <danielkong24@gmail.com>
2022-10-09 23:36:07 +0800

    All tests passed; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,29 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
 
+  NEWLINE = "\n"
+  TAB = "\t"
+
+  def row_to_hash(header, row)
+    header.zip(row).to_h
+  end
+
+  def take_tsv(tsv)
+    temp_arr = tsv.split(NEWLINE).map { |elem| elem.split(TAB) }
+    header = temp_arr.first
+    rows = temp_arr[1..]
+    @data = rows.map { |row| row_to_hash(header, row) }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    header = @data.first.keys.join(TAB)
+    tsv_string = @data.map { |elem| elem.values.join(TAB) }.join(NEWLINE)
+    header + NEWLINE + tsv_string + NEWLINE
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,19 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yml - a String in YAML format
+
+  def take_yaml(yml)
+    @data = YAML.safe_load(yml)
+  end
+
+  # to_tsv: converts @data into tsv string
+  # returns: String in TSV format
+  def to_yaml
+    @data.to_yaml
+  end
+end
